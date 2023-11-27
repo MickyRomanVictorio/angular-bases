6caa81e0b178b8ce432caba279f8f6e153348b77
Miguel Román <miguel.rv375@gmail.com>
2023-11-27 12:20:27 -0500

    Iniciando mi proyecto de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent],
+   imports: [BrowserModule, CounterModule, HeroesModule],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
